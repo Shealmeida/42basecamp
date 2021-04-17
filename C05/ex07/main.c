2dc7998c0ed2cdf1e38ce77bb39afc21ffd0079e
sde-alme <sde-alme@student.42sp.org.br>
2021-04-17 04:39:49 +0000

    C05

---
+++ C05/ex07/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n = 5;
+int	ft_find_next_prime(int nb);
+    printf("%d", ft_find_next_prime(n));
+}
+
+
+//OU
+
+//#include <stdio.h>
+
+//int main(void)
+//{
+//    int n = 5;
+//    printf("%d", ft_find_next_prime(n));
+//}
